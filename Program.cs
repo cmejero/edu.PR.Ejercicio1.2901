bc7f7b196351e7d49364a1358c1810a8b47c7cc0
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2024-02-05 12:57:42 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+using System.Reflection;
+
+namespace edu.PR.Ejercicio1._2901
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                                                        
+            DateTime fechaActual = DateTime.Now;
+            Console.WriteLine("fecha actual: " + fechaActual);
+
+            DateTimeFormatInfo infoFormatoCulturaEspecifica = CultureInfo.GetCultureInfo("es-SP").DateTimeFormat;
+            Type tipo = infoFormatoCulturaEspecifica.GetType();
+            PropertyInfo[] propiedadesFormatos = tipo.GetProperties();
+            
+            foreach (var propiedad in propiedadesFormatos)
+            {
+                
+                if (propiedad.Name.Contains("Pattern"))
+                {
+                    string formato = propiedad.GetValue(infoFormatoCulturaEspecifica, null).ToString();
+                    Console.WriteLine(propiedad.Name + ": " + formato + " " +
+                                      fechaActual.ToString(formato));
+                }
+            }
+
+            Console.WriteLine(fechaActual.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss")) ;
+
+
+
+
+
+        }
+
+
+    }
+
+
+
+}
+
+
+
